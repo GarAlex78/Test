7f7286f95fd26e12df32d32a81e967b8d48340d8
GarAlex78 <gaa197848@gmail.com>
2022-08-15 15:27:47 +0300

    add hom work 1

---
+++ js-hw-01/App/one.js
@@ -0,0 +1,36 @@
+"use strick";
+
+// const number = 20.5;
+
+// const name = "sasha";
+
+// const isActive = true;
+
+// const type = typeof name
+
+// console.log(number);
+
+// console.log(name);
+
+// console.log(isActive);
+
+// console.log(window);
+
+//alert ('ops');
+
+// const input = prompt('input your name');
+// console.log(input);
+//console.log(typeof input);
+
+// const isname = confirm('input your soname');
+// console.log(typeof isname);
+// console.log(isname);
+// const a = 50;
+// const b = 10;
+// const result = 5;
+// const isNumberInRange = result > b && result < a;
+// console.log(isNumberInRange);
+const result = 5 % 5;
+console.log(result);
+
+
